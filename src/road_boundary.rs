3bc2bd2a1e25ade4a8ad220a3c0e4b3991b8c79c
Florian Lercher <florian.lercher@tum.de>
2025-12-11 13:26:23 +0100

    Simplify road polygon to avoid artifacts

--- src/road_boundary.rs
+++ src/road_boundary.rs
@@ -1,26 +1,24 @@
-use geo::{Area, BooleanOps, ConvexHull, Polygon, Winding, unary_union};
+use geo::{Area, BooleanOps, ConvexHull, HasDimensions, Polygon, Simplify, unary_union};
 use itertools::Itertools;
-use nalgebra::{Isometry2, Point2};
-use parry2d_f64::shape::{ConvexPolygon, SharedShape, TriMesh};
+use nalgebra::Isometry2;
+use parry2d_f64::shape::{SharedShape, TriMesh};
 
 use crate::polygon::PolygonCollisionObject;
 
 pub fn create_road_boundary_obstacle(
     lanelets: &[Polygon],
 ) -> (Vec<(Isometry2<f64>, SharedShape)>, Vec<TriMesh>) {
-    let road = unary_union(lanelets);
+    let road = unary_union(lanelets).simplify(0.01); // Simplify with 1 cm tolerance to reduce artifacts
+    if road.is_empty() {
+        return (Vec::new(), Vec::new());
+    }
     let road_convex_hull = road.convex_hull();
 
     // Construct outer half-spaces from convex hull
-    let road_convex_hull_parry = ConvexPolygon::from_convex_polyline(
-        road_convex_hull
-            .exterior()
-            .points_ccw()
-            .skip(1)
-            .map(|point| Point2::new(point.x(), point.y()))
-            .collect(),
-    )
-    .expect("Convex hull is a valid convex polygon");
+    let road_convex_hull_parry = match PolygonCollisionObject::new(&road_convex_hull) {
+        Some(PolygonCollisionObject::ConvexPolygon(poly)) => poly,
+        _ => unreachable!("Convex hull should be a valid convex polygon"),
+    };
     let outer_halfspaces = itertools::izip!(
         road_convex_hull_parry.normals(),
         road_convex_hull_parry.points()
